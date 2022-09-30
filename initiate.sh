fc05c3f40eaab934b72cec6f76bed7419899544a
ptah0414 <ptah0414@naver.com>
2022-09-30 13:50:07 +0900

    removed comments

--- initiate.sh
+++ initiate.sh
@@ -1,13 +1,10 @@
 #!/bin/bash 
 
-# create k8s-master and nodes. provision k8s-master
 vagrant destroy -f \
 && vagrant up \
-# provision nodes 
 && sed '/#1/s/^/#/' Vagrantfile \
 && sed '/\#2/d' Vagrantfile \
 && vagrant provision \
-# k8s creation check 
 && vagrant ssh k8s-master \
 && kubectl get nodes \
 && kubectl get pods -n kube-system
